a70ff0dee3ce5a801ee17f919865b8f18bc4439d
Alexander Ch. <axrwrk@gmail.com>
2025-12-10 01:16:29 +0300

    Delete bot directory

--- bot/__init__.py
+++
@@ -1,2 +0,0 @@
-# bot/__init__.py
-__all__ = ["main"]
--- bot/routers/__init__.py
+++
@@ -1,6 +0,0 @@
-# bot/routers/__init__.py
-from .start import router as start_router
-from .navigation import router as navigation_router
-from .chat import router as chat_router
-
-__all__ = ["start_router", "navigation_router", "chat_router"]
