cfc4c495aee33fcbb35c19076c8abbeb39ff6b90
Alexander Ch. <axrwrk@gmail.com>
2025-12-09 18:33:29 +0300

    Delete migrations directory

--- migrations/002_planner_init.sql
+++
@@ -1,5 +0,0 @@
--- migrations/002_planner_init.sql
--- Дублирует базовую схему для совместимости с командой:
--- psql "$PLANNER_DATABASE_URL" -f migrations/002_planner_init.sql
-
-\i migrations/001_init.sql
