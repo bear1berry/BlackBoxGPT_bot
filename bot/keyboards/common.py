0570d6135f3259be764e9c14549914323ad4c903
Alexander Ch. <axrwrk@gmail.com>
2025-12-12 03:56:13 +0300

    Delete bot directory

--- bot/keyboards/common.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-def get_back_keyboard():
-    return ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="⬅️ Назад")]
-        ],
-        resize_keyboard=True
-    )
--- bot/services/__init__.py
+++
@@ -1,8 +0,0 @@
-from .llm import Mode, StyleParams, ask_llm, ask_llm_stream
-
-__all__ = [
-    "Mode",
-    "StyleParams",
-    "ask_llm",
-    "ask_llm_stream",
-]
